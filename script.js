17427e7e463bf9e0cd67403fda206015e6efb50f
TushalHansola <tusarhansola@gmail.com>
2024-12-17 13:57:47 +0530

    first commit

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+let eleC=document.querySelector(".contanier_elem");
+let fixed=document.querySelector("#fiexd_image");
+eleC.addEventListener("mouseenter",function(){
+    fixed.style.display="block";
+})
+eleC.addEventListener("mouseleave",function(){
+    fixed.style.display="none";
+});
+
+let elems=document.querySelectorAll("#elem");
+// elems.addEventListener("mouseenter",function(){
+//     let image=elems.getAttribute("data-image");
+//     fixed.style.backgroundImage=`url(${image})`;
+// })
+elems.forEach(function(a){
+  a.addEventListener("mouseenter",function(){
+      let image=a.getAttribute("data-image");
+    fixed.style.background=`url(${image})`;
+  })
+})
